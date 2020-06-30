d218d735f4f70900e3604f9420e6951780c4de55
Dara <dabedini@UCSC.edu>
2020-06-30 14:41:03 -0700

    ver 1

--- CSE115/ViewController.swift
+++ CSE115/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    
+    
+    let backgroundImageView = UIImageView()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        setBackgound()
         // Do any additional setup after loading the view.
     }
-
-
+    func setBackgound(){
+        view.addSubview(backgroundImageView)
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        
+        
+        
+        
+    }
+    
+    
+    @IBAction func signbut(_ sender: Any) {
+        
+        let MyEmail =  String(emailTxt.text!)
+        let MyPass = String(passTxt.text!)
+        if(MyPass=="" && MyEmail == " " ){
+            
+        }
+        
+    }
+    
+    @IBAction func regBut(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func GoogleBut(_ sender: Any) {
+        
+        
+        
+        
+    }
 }
-
