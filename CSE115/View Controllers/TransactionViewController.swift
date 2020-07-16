4a707d1eaa429331ebcd5e0e6e85fe5026206f8e
Dara <dabedini@UCSC.edu>
2020-07-15 18:45:28 -0700

    transaction page ver 1

--- CSE115/View Controllers/TransactionViewController.swift
+++ CSE115/View Controllers/TransactionViewController.swift
@@ -11,9 +11,30 @@ import Firebase
 
 class TransactionViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var returnDateText: UITextField!
+    @IBOutlet weak var loanDateText: UITextField!
     @IBAction func backBtnPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = UIDatePicker.Mode.date
+        datePicker.addTarget(self, action: #selector(TransactionViewController.datePickerValueChanged(sender:)), for: UIControl.Event.valueChanged)
+        loanDateText.inputView = datePicker
+        returnDateText.inputView = datePicker
+        
+    }
     
     
+    
+    @objc func datePickerValueChanged(sender: UIDatePicker){
+        let formatter  = DateFormatter()
+        formatter.dateStyle = DateFormatter.Style.medium
+        formatter.timeStyle = DateFormatter.Style.none
+        loanDateText.text = formatter.string(from: sender.date)
+        returnDateText.text = formatter.string(from: sender.date)
+}
 }
