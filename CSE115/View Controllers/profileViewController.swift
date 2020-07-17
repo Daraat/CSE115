767d3bf16cd2261e23a36abd949238fecac42195
Dara <dabedini@UCSC.edu>
2020-07-17 16:31:09 -0700

    test 4

---
+++ CSE115/View Controllers/profileViewController.swift
@@ -0,0 +1,36 @@
+//
+//  profileViewController.swift
+//  CSE115
+//
+//  Created by Dara Abedini tafreshi on 7/17/20.
+//  Copyright © 2020 Dara Abedini tafreshi. All rights reserved.
+//
+
+import UIKit
+
+
+
+class profileViewController: UIViewController{
+    
+    
+    
+    
+    
+    
+    @IBOutlet weak var profilePic: UIImageView!
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        profilePic.makeRound()
+        
+        
+        
+   
+    }
+    
+}
