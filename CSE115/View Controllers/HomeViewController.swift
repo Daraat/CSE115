e3aa646470dc99da70eaa79e5e65e73aa564b82f
Raphael <raphaelzaafrani@gmail.com>
2020-07-22 01:30:19 -0700

    Feed implemented!

--- CSE115/View Controllers/HomeViewController.swift
+++ CSE115/View Controllers/HomeViewController.swift
@@ -7,30 +7,76 @@
 //
 
 import UIKit
+import Firebase
+import SwiftKeychainWrapper
 import SideMenu
 
-struct cell{
+struct cellStruct{
     let from : String!
     let to : String!
     let item : String!
     let loanDate : String!
     let imgPath : String!
-    let owner : Bool!
 }
 
 class HomeViewController : UIViewController, UITableViewDelegate, UITableViewDataSource{
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
+    @IBOutlet weak var feedTableView: UITableView!
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
+    let dbRef = Database.database().reference().child("users")
+    let storageRef = Storage.storage().reference()
+    let handle = KeychainWrapper.standard.string(forKey: USR_HANDLE)!
+    
+    var cells = [cellStruct]()
     
     override func viewDidLoad() {
+        self.feedTableView.delegate = self
+        self.feedTableView.dataSource = self
         SideMenuManager.default.addPanGestureToPresent(toView: self.view)
+        
+        dbRef.child(handle).child("transactions").queryOrderedByKey().observe(.childAdded, with:{ (snapshot) in
+            
+            let dict = snapshot.value as! Dictionary<String, Any>
+            let item = dict["itemName"] as! String
+            let imgPath = dict["imgPath"] as! String
+            let from = dict["loanerHandle"] as! String
+            let to = dict["loaneeHandle"] as! String
+            let loanDate = dict["loanDate"] as! String
+            
+            self.cells.insert(cellStruct(from: from, to: to, item: item, loanDate: loanDate, imgPath: imgPath), at: 0)
+            self.feedTableView.reloadData()
+
+        })
         
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cells.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = feedTableView.dequeueReusableCell(withIdentifier: "transactionCell")
+        let curCell = cells[indexPath.row]
+        let img = cell?.viewWithTag(1) as! UIImageView
+        let itemLbl = cell?.viewWithTag(2) as! UILabel
+        let fromLbl = cell?.viewWithTag(3) as! UILabel
+        let toLbl = cell?.viewWithTag(4) as! UILabel
+        let dateLbl = cell?.viewWithTag(5) as! UILabel
+        
+        storageRef.child(curCell.imgPath).getData(maxSize: 1 * 2048 * 2048, completion: { data, error in
+            if let error = error {
+                print("PIC: ",error)
+            } else {
+                img.image = UIImage(data: data!)
+            }
+        })
+        
+        itemLbl.text = curCell.item
+        fromLbl.text = curCell.from
+        toLbl.text = curCell.to
+        dateLbl.text = curCell.loanDate
+        
+        return cell!
+    }
 }
